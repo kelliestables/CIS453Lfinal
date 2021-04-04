20a064e448d3c347c6d7eb1d3fafb34e534e3bc0
Kellie Stables <kellie.stables@yahoo.com>
2021-04-04 18:08:51 -0400

    adding to repository

---
+++ src/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+ 
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Reviews/Comments/Concerns?</h2>
+        <p>Please reach out to us at sushimama@sm.com.
+        </p>
+      </div>
+    );
+  }
+}
+ 
+export default Contact;
---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+ 
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Welcome to Sushi Mama</h2>
+        <p>Chain teppanyaki grill serving steaks and seafood, 
+          plus sushi and a traditional Japanese menu.</p>
+ 
+        <p>We offer dine-in, takeout, and delivery</p>
+
+        <p>Our hours are 11AM to 9:30PM Mon - Thurs.</p>
+      </div>
+    );
+  }
+}
+ 
+export default Home;
