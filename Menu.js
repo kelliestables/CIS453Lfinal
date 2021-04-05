e50557daa7343ddb6353e447009844a439ceb2e9
Kellie Stables <70247560+kelliestables@users.noreply.github.com>
2021-04-04 22:51:08 -0400

    Add files via upload

---
+++ Menu.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+ 
+class Menu extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Menu</h2>
+        <div class="container">
+  <div class="group-header">
+    
+  </div>
+  
+  <h3>
+    <span>LOBSTER FILET MIGNON ROLL</span>
+    
+  </h3>
+  <h5>18.95</h5>
+  <p>Lobster tempura and avocado rolled and topped with seared filet mignon, creamy wasabi sauce, 
+    green onions and red bell peppers; served with lobster tempura over fried zucchini and ginger teriyaki sauce</p>
+  <h3>
+    <span>SURF AND TURF ROLL</span>
+    <h5>22.95</h5> 
+  </h3>
+  <p>Spicy tuna mix, shrimp tempura and cucumber, rolled and 
+    topped with seared filet mignon, drizzled with black pepper soy sauce; 
+    finished with green onion and red bell pepper</p>
+  <h3>
+    <span>ULTIMATE SHRIMP TEMPURA ROLL</span>
+    <h5>9.95</h5>
+  </h3>
+  <p>Shrimp tempura with spicy krab
+     mix and cucumber, rolled and topped with seared tuna
+      and avocado</p>
+</div>
+      </div>
+    );
+  }
+}
+ 
+export default Menu;
